0bf0aa90c683b9bba5210d0ba99e5bc8e312bc6d
Christian Penrod <ChristianPenrod@christians-mac.local>
2017-02-03 01:38:09 -0500

    Initial Commit

---
+++ THISismyapp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  THISismyapp2
+//
+//  Created by Christian Penrod on 2/3/17.
+//  Copyright © 2017 Christian Penrod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
